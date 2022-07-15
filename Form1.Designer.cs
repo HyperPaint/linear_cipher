e02627838ba3393c3d4c3f8bb68ccc854d311a22
HyperPaint <dioman172@yandex.ru>
2022-07-15 20:21:20 +1000

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace LinearCypher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encrypt = new System.Windows.Forms.Button();
+            this.decrypt = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.key_b = new System.Windows.Forms.TextBox();
+            this.key_a = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.text = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // encrypt
+            // 
+            this.encrypt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.encrypt.Location = new System.Drawing.Point(13, 154);
+            this.encrypt.Name = "encrypt";
+            this.encrypt.Size = new System.Drawing.Size(215, 23);
+            this.encrypt.TabIndex = 0;
+            this.encrypt.Text = "Зашифровать";
+            this.encrypt.UseVisualStyleBackColor = true;
+            this.encrypt.Click += new System.EventHandler(this.encrypt_Click);
+            // 
+            // decrypt
+            // 
+            this.decrypt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.decrypt.Location = new System.Drawing.Point(12, 183);
+            this.decrypt.Name = "decrypt";
+            this.decrypt.Size = new System.Drawing.Size(216, 23);
+            this.decrypt.TabIndex = 1;
+            this.decrypt.Text = "Расшифровать";
+            this.decrypt.UseVisualStyleBackColor = true;
+            this.decrypt.Click += new System.EventHandler(this.decrypt_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.key_b);
+            this.groupBox1.Controls.Add(this.key_a);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(216, 70);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ключ";
+            // 
+            // key_b
+            // 
+            this.key_b.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.key_b.Location = new System.Drawing.Point(110, 44);
+            this.key_b.Name = "key_b";
+            this.key_b.Size = new System.Drawing.Size(100, 20);
+            this.key_b.TabIndex = 9;
+            // 
+            // key_a
+            // 
+            this.key_a.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.key_a.Location = new System.Drawing.Point(110, 19);
+            this.key_a.Name = "key_a";
+            this.key_a.Size = new System.Drawing.Size(100, 20);
+            this.key_a.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Часть ключа b";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Часть ключа a";
+            // 
+            // text
+            // 
+            this.text.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.text.Location = new System.Drawing.Point(13, 89);
+            this.text.Multiline = true;
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(215, 59);
+            this.text.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 218);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.decrypt);
+            this.Controls.Add(this.encrypt);
+            this.Name = "Form1";
+            this.Text = "Линейный шифр";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encrypt;
+        private System.Windows.Forms.Button decrypt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox key_b;
+        private System.Windows.Forms.TextBox key_a;
+        private System.Windows.Forms.TextBox text;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LinearCypher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //"АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+        private bool check()
+        {
+            bool ok = true;
+            if (!check_number(key_a.Text))
+            {
+                MessageBox.Show("Часть ключа А содержит недопустимые символы");
+                ok = false;
+            }
+            if (!check_number(key_b.Text))
+            {
+                MessageBox.Show("Часть ключа B содержит недопустимые символы");
+                ok = false;
+            }
+            if (!check_alphabet(text.Text))
+            {
+                MessageBox.Show("Текст содержит недопустимые символы");
+                ok = false;
+            }
+            return ok;
+        }
+
+        private bool check_number(string str)
+        {
+            try
+            {
+                Convert.ToInt32(str);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool check_alphabet(string str)
+        {
+            foreach (var item in str)
+            {
+                if ( !alphabet.Contains(item) )
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private string encrypt_text(string text, int a, int b)
+        {
+            int result, y;
+            string result_text = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                y = (text[i] - alphabet[0]);
+                result = (a * y + b) % alphabet.Length;
+                result_text += alphabet[result];
+            }
+            return result_text;
+        }
+
+        private string decrypt_text(string text, int a, int b)
+        {
+            if (a % 2 == 0)
+            {
+                MessageBox.Show("Часть ключа А содержит недопустимые символы");
+                return text;
+            }
+
+            int a_ = 0;
+            while ((a * a_) % alphabet.Length != 1)
+            {
+                a_++;
+            }
+
+            int result, y;
+            string result_text = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                y = (text[i] - alphabet[0]);
+                result = a_ * (y - b);
+                while (result < 0)
+                {
+                    result += alphabet.Length;
+                }
+                result = result % alphabet.Length;
+                result_text += alphabet[result];
+            }
+            return result_text;
+        }
+
+        private void encrypt_Click(object sender, EventArgs e)
+        {
+            if (check())
+            {
+                text.Text = encrypt_text(text.Text, Convert.ToInt32(key_a.Text), Convert.ToInt32(key_b.Text));
+                File.WriteAllText("text.txt", text.Text);
+            }
+        }
+
+        private void decrypt_Click(object sender, EventArgs e)
+        {
+            if (check())
+            {
+                text.Text = decrypt_text(text.Text, Convert.ToInt32(key_a.Text), Convert.ToInt32(key_b.Text));
+                File.WriteAllText("text.txt", text.Text);
+            }
+        }
+    }
+}
